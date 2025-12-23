8ee0cac040bd534e02336ef7b97f3f104d2e6943
A. Pechenkin <pechekin685@gmail.com>
2025-12-23 16:42:18 +0700

    Проверка crud

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3300',
+      apiBaseUrl: process.env.API_BASE_URL || 'http://api2.infomania.ru',
     },
   },
 });
