cd517c5b8012992f104e7b20912852e985d6028e
A. Pechenkin <pechekin685@gmail.com>
2026-02-06 14:28:39 +0700

    Исправление файлов среды

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.API_BASE_URL || 'http://api2.infomania.ru',
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE,
     },
   },
 });
