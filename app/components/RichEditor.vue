fb053a7bca6408da4e7d595fe82d96c6977d850a
A. Pechenkin <pechekin685@gmail.com>
2026-04-14 13:39:01 +0700

    Новый эдитор тестовый запуск

--- app/components/RichEditor.vue
+++ app/components/RichEditor.vue
@@ -1,217 +1,221 @@
-<script setup lang="ts">
-import type { EditorToolbarItem } from '#ui/components/EditorToolbar.vue';
-import { TableKit } from '@tiptap/extension-table';
+<!--<script setup lang="ts">-->
+<!--import type { EditorToolbarItem } from '#ui/components/EditorToolbar.vue';-->
+<!--import { TableKit } from '@tiptap/extension-table';-->
 
-const items: EditorToolbarItem[][] = [
-  [
-    {
-      kind: 'undo',
-      icon: 'i-lucide-undo',
-      tooltip: { text: 'Undo' },
-    },
-    {
-      kind: 'redo',
-      icon: 'i-lucide-redo',
-      tooltip: { text: 'Redo' },
-    },
-  ],
-  // Block types
-  [
-    {
-      icon: 'i-lucide-heading',
-      tooltip: { text: 'Headings' },
-      content: {
-        align: 'start',
-      },
-      items: [
-        {
-          kind: 'heading',
-          level: 1,
-          icon: 'i-lucide-heading-1',
-          label: 'Heading 1',
-        },
-        {
-          kind: 'heading',
-          level: 2,
-          icon: 'i-lucide-heading-2',
-          label: 'Heading 2',
-        },
-        {
-          kind: 'heading',
-          level: 3,
-          icon: 'i-lucide-heading-3',
-          label: 'Heading 3',
-        },
-        {
-          kind: 'heading',
-          level: 4,
-          icon: 'i-lucide-heading-4',
-          label: 'Heading 4',
-        },
-      ],
-    },
-    {
-      icon: 'i-lucide-list',
-      tooltip: { text: 'Lists' },
-      content: {
-        align: 'start',
-      },
-      items: [
-        {
-          kind: 'bulletList',
-          icon: 'i-lucide-list',
-          label: 'Bullet List',
-        },
-        {
-          kind: 'orderedList',
-          icon: 'i-lucide-list-ordered',
-          label: 'Ordered List',
-        },
-      ],
-    },
-    {
-      kind: 'blockquote',
-      icon: 'i-lucide-text-quote',
-      tooltip: { text: 'Blockquote' },
-    },
-    {
-      kind: 'codeBlock',
-      icon: 'i-lucide-square-code',
-      tooltip: { text: 'Code Block' },
-    },
-    {
-      kind: 'horizontalRule',
-      icon: 'i-lucide-separator-horizontal',
-      tooltip: { text: 'Horizontal Rule' },
-    },
-  ],
-  // Text formatting
-  [
-    {
-      kind: 'mark',
-      mark: 'bold',
-      icon: 'i-lucide-bold',
-      tooltip: { text: 'Bold' },
-    },
-    {
-      kind: 'mark',
-      mark: 'italic',
-      icon: 'i-lucide-italic',
-      tooltip: { text: 'Italic' },
-    },
-    {
-      kind: 'mark',
-      mark: 'underline',
-      icon: 'i-lucide-underline',
-      tooltip: { text: 'Underline' },
-    },
-    {
-      kind: 'mark',
-      mark: 'strike',
-      icon: 'i-lucide-strikethrough',
-      tooltip: { text: 'Strikethrough' },
-    },
-    {
-      kind: 'mark',
-      mark: 'code',
-      icon: 'i-lucide-code',
-      tooltip: { text: 'Code' },
-    },
-  ],
-  // Link
-  [
-    {
-      kind: 'link',
-      icon: 'i-lucide-link',
-      tooltip: { text: 'Link' },
-    },
-  ],
-];
-const extensions = [TableKit];
-</script>
+<!--const items: EditorToolbarItem[][] = [-->
+<!--  [-->
+<!--    {-->
+<!--      kind: 'undo',-->
+<!--      icon: 'i-lucide-undo',-->
+<!--      tooltip: { text: 'Undo' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'redo',-->
+<!--      icon: 'i-lucide-redo',-->
+<!--      tooltip: { text: 'Redo' },-->
+<!--    },-->
+<!--  ],-->
+<!--  // Block types-->
+<!--  [-->
+<!--    {-->
+<!--      icon: 'i-lucide-heading',-->
+<!--      tooltip: { text: 'Headings' },-->
+<!--      content: {-->
+<!--        align: 'start',-->
+<!--      },-->
+<!--      items: [-->
+<!--        {-->
+<!--          kind: 'heading',-->
+<!--          level: 1,-->
+<!--          icon: 'i-lucide-heading-1',-->
+<!--          label: 'Heading 1',-->
+<!--        },-->
+<!--        {-->
+<!--          kind: 'heading',-->
+<!--          level: 2,-->
+<!--          icon: 'i-lucide-heading-2',-->
+<!--          label: 'Heading 2',-->
+<!--        },-->
+<!--        {-->
+<!--          kind: 'heading',-->
+<!--          level: 3,-->
+<!--          icon: 'i-lucide-heading-3',-->
+<!--          label: 'Heading 3',-->
+<!--        },-->
+<!--        {-->
+<!--          kind: 'heading',-->
+<!--          level: 4,-->
+<!--          icon: 'i-lucide-heading-4',-->
+<!--          label: 'Heading 4',-->
+<!--        },-->
+<!--      ],-->
+<!--    },-->
+<!--    {-->
+<!--      icon: 'i-lucide-list',-->
+<!--      tooltip: { text: 'Lists' },-->
+<!--      content: {-->
+<!--        align: 'start',-->
+<!--      },-->
+<!--      items: [-->
+<!--        {-->
+<!--          kind: 'bulletList',-->
+<!--          icon: 'i-lucide-list',-->
+<!--          label: 'Bullet List',-->
+<!--        },-->
+<!--        {-->
+<!--          kind: 'orderedList',-->
+<!--          icon: 'i-lucide-list-ordered',-->
+<!--          label: 'Ordered List',-->
+<!--        },-->
+<!--      ],-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'blockquote',-->
+<!--      icon: 'i-lucide-text-quote',-->
+<!--      tooltip: { text: 'Blockquote' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'codeBlock',-->
+<!--      icon: 'i-lucide-square-code',-->
+<!--      tooltip: { text: 'Code Block' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'horizontalRule',-->
+<!--      icon: 'i-lucide-separator-horizontal',-->
+<!--      tooltip: { text: 'Horizontal Rule' },-->
+<!--    },-->
+<!--  ],-->
+<!--  // Text formatting-->
+<!--  [-->
+<!--    {-->
+<!--      kind: 'mark',-->
+<!--      mark: 'bold',-->
+<!--      icon: 'i-lucide-bold',-->
+<!--      tooltip: { text: 'Bold' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'mark',-->
+<!--      mark: 'italic',-->
+<!--      icon: 'i-lucide-italic',-->
+<!--      tooltip: { text: 'Italic' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'mark',-->
+<!--      mark: 'underline',-->
+<!--      icon: 'i-lucide-underline',-->
+<!--      tooltip: { text: 'Underline' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'mark',-->
+<!--      mark: 'strike',-->
+<!--      icon: 'i-lucide-strikethrough',-->
+<!--      tooltip: { text: 'Strikethrough' },-->
+<!--    },-->
+<!--    {-->
+<!--      kind: 'mark',-->
+<!--      mark: 'code',-->
+<!--      icon: 'i-lucide-code',-->
+<!--      tooltip: { text: 'Code' },-->
+<!--    },-->
+<!--  ],-->
+<!--  // Link-->
+<!--  [-->
+<!--    {-->
+<!--      kind: 'link',-->
+<!--      icon: 'i-lucide-link',-->
+<!--      tooltip: { text: 'Link' },-->
+<!--    },-->
+<!--  ],-->
+<!--];-->
+<!--const extensions = [TableKit];-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <UEditor v-slot="{ editor }" :extensions="extensions">-->
+<!--    <UButton-->
+<!--      @click="-->
+<!--        editor-->
+<!--          .chain()-->
+<!--          .focus()-->
+<!--          .insertTable({ rows: 3, cols: 3, withHeaderRow: true })-->
+<!--          .run()-->
+<!--      "-->
+<!--    >-->
+<!--      Insert table-->
+<!--    </UButton>-->
+<!--    <UEditorToolbar :editor="editor" :items="items" />-->
+<!--  </UEditor>-->
+<!--</template>-->
+
+<!--<style scoped>-->
+<!--.tiptap {-->
+<!--  :first-child {-->
+<!--    margin-top: 0;-->
+<!--  }-->
+
+<!--  /* Table-specific styling */-->
+<!--  table {-->
+<!--    border-collapse: collapse;-->
+<!--    margin: 0;-->
+<!--    overflow: hidden;-->
+<!--    table-layout: fixed;-->
+<!--    width: 100%;-->
+
+<!--    td,-->
+<!--    th {-->
+<!--      border: 1px solid var(&#45;&#45;gray-3);-->
+<!--      box-sizing: border-box;-->
+<!--      min-width: 1em;-->
+<!--      padding: 6px 8px;-->
+<!--      position: relative;-->
+<!--      vertical-align: top;-->
+
+<!--      > * {-->
+<!--        margin-bottom: 0;-->
+<!--      }-->
+<!--    }-->
+
+<!--    th {-->
+<!--      background-color: var(&#45;&#45;gray-1);-->
+<!--      font-weight: bold;-->
+<!--      text-align: left;-->
+<!--    }-->
+
+<!--    .selectedCell:after {-->
+<!--      background: var(&#45;&#45;gray-2);-->
+<!--      content: '';-->
+<!--      left: 0;-->
+<!--      right: 0;-->
+<!--      top: 0;-->
+<!--      bottom: 0;-->
+<!--      pointer-events: none;-->
+<!--      position: absolute;-->
+<!--      z-index: 2;-->
+<!--    }-->
+
+<!--    .column-resize-handle {-->
+<!--      background-color: var(&#45;&#45;purple);-->
+<!--      bottom: -2px;-->
+<!--      pointer-events: none;-->
+<!--      position: absolute;-->
+<!--      right: -2px;-->
+<!--      top: 0;-->
+<!--      width: 4px;-->
+<!--    }-->
+<!--  }-->
+
+<!--  .tableWrapper {-->
+<!--    margin: 1.5rem 0;-->
+<!--    overflow-x: auto;-->
+<!--  }-->
+
+<!--  &.resize-cursor {-->
+<!--    cursor: ew-resize;-->
+<!--    cursor: col-resize;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
 
 <template>
-  <UEditor v-slot="{ editor }" :extensions="extensions">
-    <UButton
-      @click="
-        editor
-          .chain()
-          .focus()
-          .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
-          .run()
-      "
-    >
-      Insert table
-    </UButton>
-    <UEditorToolbar :editor="editor" :items="items" />
-  </UEditor>
+  <div>Эдитор</div>
 </template>
-
-<style scoped>
-.tiptap {
-  :first-child {
-    margin-top: 0;
-  }
-
-  /* Table-specific styling */
-  table {
-    border-collapse: collapse;
-    margin: 0;
-    overflow: hidden;
-    table-layout: fixed;
-    width: 100%;
-
-    td,
-    th {
-      border: 1px solid var(--gray-3);
-      box-sizing: border-box;
-      min-width: 1em;
-      padding: 6px 8px;
-      position: relative;
-      vertical-align: top;
-
-      > * {
-        margin-bottom: 0;
-      }
-    }
-
-    th {
-      background-color: var(--gray-1);
-      font-weight: bold;
-      text-align: left;
-    }
-
-    .selectedCell:after {
-      background: var(--gray-2);
-      content: '';
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      pointer-events: none;
-      position: absolute;
-      z-index: 2;
-    }
-
-    .column-resize-handle {
-      background-color: var(--purple);
-      bottom: -2px;
-      pointer-events: none;
-      position: absolute;
-      right: -2px;
-      top: 0;
-      width: 4px;
-    }
-  }
-
-  .tableWrapper {
-    margin: 1.5rem 0;
-    overflow-x: auto;
-  }
-
-  &.resize-cursor {
-    cursor: ew-resize;
-    cursor: col-resize;
-  }
-}
-</style>
